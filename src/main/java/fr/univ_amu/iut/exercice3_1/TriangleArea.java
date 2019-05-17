0e5e39c6ce3fd449a81c6170f1b2081e759d497d
Wowsnake <steffen.alvarez@etu.univ-amu.fr>
2019-05-17 17:31:17 +0200

    exo 3.1 tp3

--- src/main/java/fr/univ_amu/iut/exercice3_1/TriangleArea.java
+++ src/main/java/fr/univ_amu/iut/exercice3_1/TriangleArea.java
@@ -1,6 +1,7 @@
 package fr.univ_amu.iut.exercice3_1;
 
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.binding.NumberBinding;
 import javafx.beans.binding.StringExpression;
 import javafx.beans.property.IntegerProperty;
@@ -63,10 +64,14 @@ public class TriangleArea {
     }
 
     void printResult() {
-        throw new RuntimeException("Not yet implemented !");
+        //System.out.println("For P1(" + x1.get() + "," + y1.get() + "), " + "P2(" + x2.get() + "," + y2.get() + "), " + "P3(" + x3.get() + "," + y3.get() + "), " + "the area of triangle ABC is " + area.getValue());
+        StringExpression output = Bindings.format("For P1(%d,%d), P2(%d,%d), P3(%d,%d), the area of triangle ABC is %.1f", x1.get(), y1.get(), x2.get(), y2.get(), x3.get(), y3.get(), area.getValue());
+        System.out.println(output.getValue());
+        //throw new RuntimeException("Not yet implemented !");
     }
 
     private void createBinding() {
-        throw new RuntimeException("Not yet implemented !");
+        area = x1.multiply(y2).subtract(x1.multiply(y3)).add(x2.multiply(y3)).subtract(x2.multiply(y1)).add(x3.multiply(y1)).subtract(x3.multiply(y2)).divide(2.0);
+        //throw new RuntimeException("Not yet implemented !");
     }
 }
